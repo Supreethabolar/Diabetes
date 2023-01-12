684f4fa8f93acad0c3355733a26fb6dcdf5e1ba0
Radhakrishna <122271283+Supreethabolar@users.noreply.github.com>
2023-01-12 12:41:24 +0530

    Update diab.py

--- diab.py
+++ diab.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # Load the saved model
-loaded_model = pickle.load(open('C:/Users/Mangalore University/Downloads/trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_model.sav', 'rb'))
 
 # Create a function for Prediction
 def diabetes_prediction(input_data):
